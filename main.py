20bdef1432f8ad02818248ce373c8880a347d1c4
alexcandrkazakov <124293258+alexcandrkazakov@users.noreply.github.com>
2023-02-14 11:08:15 +0300

    14.02.2023 практическая 1

---
+++ main.py
@@ -0,0 +1,12 @@
+tseloe=10
+drobnoe=8.4
+stroka="No"
+big_netseloe=tseloe*3.5
+drobnoe=drobnoe-1
+tseloe/drobnoe
+big_netseloe/drobnoe
+stroka=stroka+"NoNo_YesYesYes"
+print(tseloe)
+print(drobnoe)
+print(big_netseloe)
+print(stroka)
