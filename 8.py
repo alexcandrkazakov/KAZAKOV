bf3dd587775d2be95e9e09583e90fab70153055b
alexcandrkazakov <124293258+alexcandrkazakov@users.noreply.github.com>
2023-03-03 11:46:01 +0300

    03.03.2023 задание 8

---
+++ 8.py
@@ -0,0 +1,46 @@
+x=int(input("Введите четырехзначное число "))
+if 999<x<10000:
+     a=x//1000
+     b=x//100 % 10 
+     c=x//10 % 10
+     d=x % 10 
+else: print("error")
+if a+b==c+d:
+    print("да")
+else: print ("нет")
+  
+  x=int(input("Введите четырехзначное число "))
+if 999<x<10000:
+     a=x//1000
+     b=x//100 % 10 
+     c=x//10 % 10
+     d=x % 10 
+else: print("error")
+if (a+b+c+d)%3==0:
+    print("да")
+else: print ("нет")
+    
+    x=int(input("Введите четырехзначное число "))
+if 999<x<10000:
+     a=x//1000
+     b=x//100 % 10 
+     c=x//10 % 10
+     d=x % 10 
+else: print("error")
+if (a*b*c*d)%4==0:
+    print("да")
+else: print ("нет")
+  
+  x=int(input("Введите четырехзначное число "))
+a=int(input("Введите число "))
+if 999<x<10000:
+     o=x//1000
+     b=x//100 % 10 
+     c=x//10 % 10
+     d=x % 10 
+else: print("error")
+if (o*b*c*d)%a==0:
+    print("да")
+else: print ("нет")
+    
+    
